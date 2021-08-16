ca279e3a24de24570abdfcb06634a51e7398abda
James Han <jameshan20024@gmail.com>
2021-08-15 22:12:20 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,8 +12,13 @@ module.exports = {
         "https://d1x26sjkwh9vok.cloudfront.net/uploads/profile/20210318/6d4b2890-31a2-4ce1-b600-7f17a7ed555d.png"
       )
       .addFields(
+        { name: "!help", value: "Shows all commands you can use!" },
         { name: "!ping", value: "returns pong!" },
-        { name: "!server", value: "Shows the server status" }
+        { name: "!server", value: "Shows the server status" },
+        {
+          name: "!study",
+          value: "Study motivation by timing how much you have studied!",
+        }
       );
 
     message.channel.send({ embeds: [exampleEmbed] });
